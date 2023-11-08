be34066f766c746ff06b8599ff24d222bf32a48f
zaynlannister <zaynlannister@gmail.com>
2023-11-08 13:41:10 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainContainer from "./components/MainContainer/MainContainer";
+
+const App = () => {
+  return (
+    <div>
+      <MainContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MainContainer/MainContainer.tsx
@@ -0,0 +1,6 @@
+import "./main.css";
+const MainContainer = () => {
+  return <div className="main custom-font">some text</div>;
+};
+
+export default MainContainer;
